42be952512656bad44eb4c814aac0c33995f0dfc
nabanitab <nababitabapari3149@gmail.com>
2024-01-18 21:42:51 +0530

    some code

---
+++ src/coreJavaPracrics/ArrayCreation.java
@@ -0,0 +1,16 @@
+package coreJavaPracrics;
+
+import java.util.Arrays;
+
+public class ArrayCreation {
+    public static void main(String[] args) {
+        //int []  x = new int[10];
+
+        int x [] = new int[30];
+        System.out.printf(Arrays.toString(x));
+
+
+    }
+
+
+}
