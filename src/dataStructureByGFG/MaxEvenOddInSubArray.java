bf9a0bac953afca8fd23c57eaef6c9c9a176402b
nabanitab <nababitabapari3149@gmail.com>
2024-02-08 23:58:11 +0530

    MaxEvenOddInSubArray in array

---
+++ src/dataStructureByGFG/MaxEvenOddInSubArray.java
@@ -0,0 +1,33 @@
+package dataStructureByGFG;
+
+public class MaxEvenOddInSubArray {
+    static int maxEvenOdd(int arr[], int n)
+    {
+        int res = 1;
+        int curr = 1;
+
+        for(int i = 1; i < n; i++)
+        {
+            if((arr[i] % 2 == 0 && arr[i - 1] % 2 != 0)
+                    ||(arr[i] % 2 != 0 && arr[i - 1] % 2 == 0))
+            {
+                curr++;
+
+                res = Math.max(res, curr);
+            }
+            else
+                curr = 1;
+        }
+
+        return res;
+    }
+
+
+    public static void main(String args[])
+    {
+        int arr[] = {5, 10, 20, 6, 3, 8}, n = 6;
+
+        System.out.println(maxEvenOdd(arr, n));
+
+    }
+}
