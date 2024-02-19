2a46d9ab5f2cf8dc856095cd8eeda5f8a5c5e300
nabanitab <nababitabapari3149@gmail.com>
2024-02-19 20:39:32 +0530

    multi threading example by extending thread class

---
+++ src/multiTheading/ExtendingThreadClass.java
@@ -0,0 +1,10 @@
+package multiTheading;
+
+public class ExtendingThreadClass extends Thread {
+    public void run(){
+        for(int i =0; i<10; i++){
+            System.out.println("child thread");
+        }
+    }
+
+}
