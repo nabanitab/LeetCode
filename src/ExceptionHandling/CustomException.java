dc5d5842c705a28c84924baf4a2308ce8b0cdc89
nabanitab <nababitabapari3149@gmail.com>
2024-02-20 20:48:51 +0530

    custom exception

---
+++ src/ExceptionHandling/CustomException.java
@@ -0,0 +1,21 @@
+package ExceptionHandling;
+
+public class CustomException extends Exception{
+
+    public CustomException (String massage){
+        super(massage);
+    }
+
+    public static void main(String[] args) {
+        int x = 0;
+        try{
+            if(x<=18){
+                throw new CustomException("eligible for voting ");
+            }
+            System.out.println("not eligible for voting");
+        }catch (CustomException ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
