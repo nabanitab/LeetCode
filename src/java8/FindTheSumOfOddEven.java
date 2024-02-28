2806ebb521e8cecba59b7033c5a72efa6dab34a2
nabanitab <nabanitabapari@ca-one.com>
2024-02-28 16:28:32 +0530

    FindTheSumOfOddEven using java 8 feature stream

---
+++ src/java8/FindTheSumOfOddEven.java
@@ -0,0 +1,20 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FindTheSumOfOddEven {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(3, 5, 6, 8, 9));
+
+        int sumOfOdd = list.stream().filter(n -> n%2 !=0).mapToInt(Integer::intValue).sum();
+
+        int sumOfEven = list.stream().filter(n -> n%2 ==0).mapToInt(Integer::intValue).sum();
+
+        System.out.println("sum of odd number in given string is : " +sumOfOdd);
+
+        System.out.println("sum of even number in given string is : " +sumOfEven);
+    }
+}
