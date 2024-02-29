a387cfa653577da3a6423eb8803cc964dea2bf04
nabanitab <nabanitabapari@ca-one.com>
2024-02-29 18:17:41 +0530

    FirstNonRepeatingChar & CharacterFrequency using hashmap

---
+++ src/hashmap/FirstNonRepeatingChar.java
@@ -0,0 +1,28 @@
+package hashmap;
+
+import java.util.*;
+//Given a string, find the first non-repeating character and return its index. If no such character exists, return -1
+
+public class FirstNonRepeatingChar {
+    public static int firstNonRepeatingChar(String str) {
+        Map<Character, Integer> countMap = new LinkedHashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            countMap.put(ch, countMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                return str.indexOf(entry.getKey());
+            }
+        }
+
+        return -1; // No non-repeating character found
+    }
+
+    public static void main(String[] args) {
+        String str = "leetcode";
+        int index = firstNonRepeatingChar(str);
+        System.out.println("Index of first non-repeating character: " + index);
+    }
+}
