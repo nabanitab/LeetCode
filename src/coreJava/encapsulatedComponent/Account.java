a674c4f474890dd58384d1b3971f2a20372c3b1e
nabanitab <nababitabapari3149@gmail.com>
2024-03-04 22:18:31 +0530

    encapsulation

---
+++ src/coreJava/encapsulatedComponent/Account.java
@@ -0,0 +1,14 @@
+package coreJava.encapsulatedComponent;
+
+public class Account {
+
+    private double balace;
+
+    public double getBalace() {
+        return balace;
+    }
+
+    public void setBalace(double balace) {
+        this.balace = balace;
+    }
+}
