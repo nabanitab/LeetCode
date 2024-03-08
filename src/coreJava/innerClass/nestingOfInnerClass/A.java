d8dd75562859daa7b97a553e14fdb17e27414696
nabanitab <nababitabapari3149@gmail.com>
2024-03-08 23:32:19 +0530

    InnerClass

---
+++ src/coreJava/innerClass/nestingOfInnerClass/A.java
@@ -0,0 +1,14 @@
+package coreJava.innerClass.nestingOfInnerClass;
+
+public class A {
+    public class B {
+       public class C {
+            public void m1() {
+                System.out.println("InnerNestedClassMethod");
+            }
+        }
+
+    }
+}
+
+
