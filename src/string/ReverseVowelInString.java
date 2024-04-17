d909e02a45e7940960759bc270dcf8aaeed6bcd5
nabanitab <nababitabapari3149@gmail.com>
2024-04-18 01:51:12 +0530

    Reverse Vowel In String

--- src/string/ReverseVowelInString.java
+++ src/string/ReverseVowelInString.java
@@ -1,23 +1,48 @@
 package string;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 //Q. Reverse only the vowels in a String
+import java.util.HashSet;
+import java.util.Set;
+
 public class ReverseVowelInString {
 
     public static void main(String[] args) {
 
-    //output will be Sasmitu
-        String sb   = "Susmita";
+        String str = "Susmita";
 
+        System.out.println(getReverseVowel(str));
     }
 
-    private static String getReverseVowel(){
+    private static String getReverseVowel(String str) {
 
-   //     Set<Character>HashSet = new HashSet<>(new ArrayList<>("a,e,i,o,u"))
-        return null;
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
+
+        StringBuilder sb = new StringBuilder();
+        // Iterate over the characters of the string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            // Check if the character is a vowel
+            if (vowels.contains(ch)) {
+                // Append the vowel to the StringBuilder
+                sb.append(ch);
+            }
+        }
+
+        // Return the reversed vowels
+        return sb.toString();
     }
 }
