610ce4a3f72db8f5d2c81f4a3c40b059724994e7
nabanitab <nababitabapari3149@gmail.com>
2024-04-28 16:04:10 +0530

    SubstringCounter

--- src/interviews/Test.java
+++ src/interviews/Test.java
@@ -2,6 +2,25 @@ package interviews;
 
 public class Test {
     public static void main(String[] args) {
-        StringObjectCreationExp cloneClass = new StringObjectCreationExp();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] res = getTargetedSum(nums, target);
+        for (int num : res) {
+            System.out.println(num);
+        }
+    }
+
+    private static int[] getTargetedSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = nums[i];
+                    result[1] = nums[j];
+                    return result;
+                }
+            }
+        }
+        return result;
     }
 }
