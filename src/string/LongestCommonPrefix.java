7c5d332d7f22dc7047aa4753d2f0cfd40c869db1
nabanitab <nababitabapari3149@gmail.com>
2024-05-04 23:41:52 +0530

    LongestCommonPrefix

---
+++ src/string/LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+package string;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String [] str = {"flore","flower","floth"};
+
+
+    }
+
+    private static String longestEle(String[] str){
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(str);
+
+        //Cha
+        return null;
+    }
+}
