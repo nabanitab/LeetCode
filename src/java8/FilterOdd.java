768c0feb214a99a7e10423a4ff595aff05b31383
nabanitab <nababitabapari3149@gmail.com>
2024-05-13 00:10:00 +0530

    FilterOdd

--- src/java8/FilterOdd.java
+++ src/java8/FilterOdd.java
@@ -2,14 +2,13 @@ package java8;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class FilterOdd {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(3, 5, 6, 8, 9));
 
-        int result = list.stream().filter(n-> n% 2==0).mapToInt(Integer::intValue).sum();
+        int result = list.stream().filter(n -> n % 2 != 0).mapToInt(Integer::intValue).sum();
         System.out.println(result);
     }
 }
