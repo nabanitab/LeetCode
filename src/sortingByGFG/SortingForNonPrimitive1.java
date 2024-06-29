c263f8a64fbfae9d2abeaa0ffb264dd4e44711cd
nabanitab <nababitabapari3149@gmail.com>
2024-06-29 22:46:20 +0530

    SortingForNonPrimitive1

---
+++ src/sortingByGFG/SortingForNonPrimitive1.java
@@ -0,0 +1,11 @@
+package sortingByGFG;
+
+import java.util.Arrays;
+
+public class SortingForNonPrimitive1 {
+    public static void main(String[] args) {
+        int arr[] ={5,30,20,10,8};
+        Arrays.sort(arr,1,4);
+        System.out.println(Arrays.toString(arr));
+    }
+}
