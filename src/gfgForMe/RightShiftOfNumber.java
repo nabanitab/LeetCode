0a4dc4689591b96a805c55173828be502f808aaa
nabanitab <nababitabapari3149@gmail.com>
2025-01-02 00:02:16 +0530

    RightShiftOfNumber

---
+++ src/gfgForMe/RightShiftOfNumber.java
@@ -0,0 +1,16 @@
+package gfgForMe;
+
+public class RightShiftOfNumber {
+
+    public static void main(String[] args) {
+        int x = 33;
+        System.out.println(x>>1);
+
+        //it is works in binary
+
+        //x     : 00...100001
+        // x>>1 : 00...010000
+
+        //o/p :16
+    }
+}
