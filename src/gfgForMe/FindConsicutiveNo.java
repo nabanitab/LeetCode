1a852530598870b62234e01ca7271d1e63ebcc31
nabanitab <nababitabapari3149@gmail.com>
2025-01-18 19:26:25 +0530

    FindConsicutiveNo

---
+++ src/gfgForMe/FindConsicutiveNo.java
@@ -0,0 +1,22 @@
+package gfgForMe;
+
+import java.util.Scanner;
+
+//1,2,3,4,5,6 : 1 o/p
+public class FindConsicutiveNo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(nthConsicutive(a,d,n));
+    }
+
+    public static int nthConsicutive(int a, int d, int n){
+        int ans = a+(n-1)*d;
+
+        return ans;
+    }
+}
