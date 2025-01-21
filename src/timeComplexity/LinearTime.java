679e214408e357be5208e34f55c49804ae9882c3
nabanitab <nababitabapari3149@gmail.com>
2025-01-21 23:54:43 +0530

    practice TimeComplexity

---
+++ src/timeComplexity/LinearTime.java
@@ -0,0 +1,13 @@
+package timeComplexity;
+
+public class LinearTime {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int num : arr) {
+            System.out.println(num); // O(n)
+        }
+    }
+}
+
+
---
+++ src/timeComplexity/PrintingSingleValue.java
@@ -0,0 +1,8 @@
+package timeComplexity;
+
+public class PrintingSingleValue {
+    public static void main(String[] args) {
+        int n = 100;
+        System.out.println(n);
+    }
+}
